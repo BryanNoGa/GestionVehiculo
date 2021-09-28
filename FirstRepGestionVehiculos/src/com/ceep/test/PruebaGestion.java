aff350383e18dcc86ffae81dc9fe5190c57d89c2
BryanNoGa <brayannoga@gmail.com>
2021-09-28 11:53:22 +0200

    Importar paquetes

---
+++ FirstRepGestionVehiculos/src/com/ceep/test/PruebaGestion.java
@@ -0,0 +1,12 @@
+package com.ceep.test;
+
+import com.ceep.domain.Vehiculos;
+
+public class PruebaGestion {
+
+    public static void main(String[] args) {
+        Vehiculos v1  = new Vehiculos("sdf-551","Honda","Civic","Gris",30000,true);
+        System.out.println(v1);
+    }
+    
+}
